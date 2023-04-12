3113a46a574af29e77763865758a11cf3e4bb1c3
Sergio Belda <sbeldagalbis@gmail.com>
2023-04-12 23:15:36 +0200

    Migrate groovy files to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CameraX-Samples"
+
+include(":camerax-app")
+include(":camerax-mlkit-app")
